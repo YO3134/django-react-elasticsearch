17fc6d36083568ab6f0bbe75ba4de74e4bbce482
YO3134 <nishiura.145@gmail.com>
2020-12-05 22:44:38 +0900

    Edit bookserach/models.py

--- backend/booksearch/models.py
+++ backend/booksearch/models.py
@@ -1,4 +1,9 @@
 from django.db import models
 
+
 class Booksearch(models.Model):
-  title = models.CharField
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+
+    def _str_(self):
+        return self.title
