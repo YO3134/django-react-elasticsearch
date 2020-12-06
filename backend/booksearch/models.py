29268e0a47c209c67a1fa162996249cd434c6d86
YO3134 <nishiura.145@gmail.com>
2020-12-06 23:13:20 +0900

    in the middle of design bookserach/models.py book class

--- backend/booksearch/models.py
+++ backend/booksearch/models.py
@@ -1,9 +1,17 @@
 from django.db import models
 
 
-class Booksearch(models.Model):
+class Book(models.Model):
+    # 暫定DB　
     title = models.CharField(max_length=120)
+    author = models.CharField(max_length=120)
+    price = models.IntegerField(null=True, blank=True)
+    publisher_name = models.CharField(max_length=120)
     description = models.TextField()
+    isbn =　models.TextField()
+    stock_status = models.IntegerField()
+    out_of_stock_flag = models.IntegerField()
+    
 
     def _str_(self):
         return self.title
