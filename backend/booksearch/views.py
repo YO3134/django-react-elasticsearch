8996e29c65806485dd1e0279d83835f2789ddb79
YO3134 <nishiura.145@gmail.com>
2020-12-07 22:18:02 +0900

    add file booksearch/views.py

--- backend/booksearch/views.py
+++ backend/booksearch/views.py
@@ -1,3 +1,12 @@
 from django.shortcuts import render
+from rest_framework import viewsets
+from .serializers import BookSerializer
+from .models import Book
+
+
+class BookView(viewsets.ModelViewSet):
+    serializer_class = BookSerializer
+    queryset = Book.objects.all()
+
 
 # Create your views here.
