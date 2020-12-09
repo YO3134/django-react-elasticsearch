dc5954f26cfb892445c03a50cf02d3691a3709b6
YO3134 <nishiura.145@gmail.com>
2020-12-09 22:39:29 +0900

    read appID from localsetting.py

--- backend/backend/get_books_from_rakuten.py
+++ backend/backend/get_books_from_rakuten.py
@@ -1,17 +1,18 @@
+import local_settings
 import requests
 import json
 import time
 
-URL = "https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?[parameter]=[value]…
-"
+URL = "https://app.rakuten.co.jp/services/api/BooksBook/Search/20170404?[parameter]=[value]…"
+appID = local_settings.APPLICATION_ID
+
 
 payload = {
-    'fornmat' : 'json',
-    'applicationID' : 'XXXXXXXXXXX',
-    'bookGenreID' : '000',
-    'keyword' : 'Django',
-    'page' : '1',
+    "fornmat": "json",
+    "applicationID": format(appID),
+    "bookGenreID": "000",
+    "keyword": "Django",
+    "page": "1",
 }
 
-books = []
-
+print(payload)
