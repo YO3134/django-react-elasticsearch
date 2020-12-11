5f1ab902d6475da4b1f30f3fe5a9f226fa9e8c89
YO3134 <nishiura.145@gmail.com>
2020-12-11 22:09:55 +0900

    Edit Book class of models.py

--- backend/booksearch/models.py
+++ backend/booksearch/models.py
@@ -2,16 +2,20 @@ from django.db import models
 
 
 class Book(models.Model):
-    # 暫定DB　
-    title = models.CharField(max_length=120)
-    author = models.CharField(max_length=120)
+    # 暫定DB
+    class Meta:
+        db_table = "book"
+        ordering = ["title", "author"]
+
+    title = models.CharField(max_length=225)
+    author = models.ForeignKey("Author", on_delete=models.SET_NULL, null=True)
     price = models.IntegerField(null=True, blank=True)
     publisher_name = models.CharField(max_length=120)
+    isbn = models.CharField("ISBN", max_lenght=13, unique=True)
+    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
     description = models.TextField()
-    isbn =　models.TextField()
     stock_status = models.IntegerField()
     out_of_stock_flag = models.IntegerField()
-    
 
     def _str_(self):
         return self.title
