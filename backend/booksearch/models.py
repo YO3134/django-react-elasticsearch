006a473957b092561a8be0c704298905b4c4859e
YO3134 <nishiura.145@gmail.com>
2020-12-11 22:22:26 +0900

    Add Genre class of models.py

--- backend/booksearch/models.py
+++ backend/booksearch/models.py
@@ -1,6 +1,16 @@
 from django.db import models
 
 
+class Gen(models.Model):
+    name = models.CharField(
+        max_length=180, elp_text="本のジャンルを入力してください(SF,時代小説,古典,ファッション)"
+    )
+
+
+def _str_(self):
+    return self.name
+
+
 class Book(models.Model):
     # 暫定DB
     class Meta:
@@ -12,7 +22,7 @@ class Book(models.Model):
     price = models.IntegerField(null=True, blank=True)
     publisher_name = models.CharField(max_length=120)
     isbn = models.CharField("ISBN", max_lenght=13, unique=True)
-    genre = models.ManyToManyField(Genre, help_text="Select a genre for this book")
+    genre = models.ManyToManyField(Genre, help_text="この本のジャンルを選択してください")
     description = models.TextField()
     stock_status = models.IntegerField()
     out_of_stock_flag = models.IntegerField()
