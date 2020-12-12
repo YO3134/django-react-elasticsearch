ceaecd6656f3769551b947400f9ffe5edb6e47e0
YO3134 <nishiura.145@gmail.com>
2020-12-12 21:01:08 +0900

    Add Author class of models.py

--- backend/booksearch/models.py
+++ backend/booksearch/models.py
@@ -3,18 +3,26 @@ from django.db import models
 
 class Genre(models.Model):
     name = models.CharField(
-        max_length=180, elp_text="本のジャンルを入力してください(SF,時代小説,古典,ファッション)"
+        max_length=180, help_text="本のジャンルを入力してください(SF,時代小説,古典,ファッション等)"
     )
 
+    def __str__(self):
+        return self.name
 
-def _str_(self):
-    return self.name
+
+class Language(models.Model):
+    name = models.CharField(max_length=200, help_text="本の言語を決定します。(日本語、英語等)")
+
+    def __str__(self):
+        return self.name
 
 
 class Book(models.Model):
     # 暫定DB
     class Meta:
+        # テーブル名
         db_table = "book"
+        # テーブル項目
         ordering = ["title", "author"]
 
     title = models.CharField(max_length=225)
@@ -27,5 +35,18 @@ class Book(models.Model):
     stock_status = models.IntegerField()
     out_of_stock_flag = models.IntegerField()
 
-    def _str_(self):
+    def __str__(self):
         return self.title
+
+
+class Author(models.MOdel):
+    first_name = models.CharField(max_length=100)
+    last_name = models.CharField(max_length=100)
+    date_of_birth = models.DateField(null=True, blank=True)
+    date_of_death = models.DateField("died", null=True, blank=True)
+
+    class Meta:
+        ordering = ["last_name", "first_name"]
+
+    def __str__(self):
+        return self.name
