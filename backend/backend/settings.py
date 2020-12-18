96c6ea7f814a5cec4366684e511913150e309ac2
YO3134 <nishiura.145@gmail.com>
2020-12-18 21:07:46 +0900

    add postgresql setting to settings.py

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -77,13 +77,24 @@ WSGI_APPLICATION = "backend.wsgi.application"
 # Database
 # https://docs.djangoproject.com/en/3.1/ref/settings/#databases
 
+# DATABASES = {
+#     "default": {
+#         "ENGINE": "django.db.backends.sqlite3",
+#         "NAME": BASE_DIR / "db.sqlite3",
+#     }
+# }
+
+# postgresql使用予定
 DATABASES = {
     "default": {
-        "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "ENGINE": "django.db.backends.postgresql_psycopg2",
+        "NAME": "myapp",
+        "USER": "USERNAME",
+        "PASSWORD": "PASSWORD",
+        "HOST": "localhost",
+        "PORT": "8000",
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/3.1/ref/settings/#auth-password-validators
